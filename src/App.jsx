b9fbef88071c7e093db2642ee9365d8409a02eda
Ragnar Pálsson <raggipals@gmail.com>
2023-10-09 16:56:48 +0000

    Finished ex 7.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import {
     BrowserRouter as Router,
-    Routes, Route, Link
+    Routes, Route, Link, Navigate
 } from "react-router-dom";
 import Footer from './components/Footer.jsx'
 import Menu from './components/Menu.jsx'
@@ -9,6 +9,7 @@ import AnecdoteList from "./components/AnecdoteList.jsx";
 import About from './components/About.jsx'
 import CreateNew from "./components/CreateNew.jsx";
 import Anecdote from "./components/Anecdote.jsx";
+import Notification from "./components/Notification.jsx";
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
@@ -33,6 +34,10 @@ const App = () => {
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000)
     setAnecdotes(anecdotes.concat(anecdote))
+    setNotification(`${anecdote.content} created successfully`)
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
   }
 
   const anecdoteById = (id) =>
@@ -56,9 +61,9 @@ const App = () => {
       <Menu />
       <Routes>
         <Route path="/about" element={<About/>}/>
-        <Route path="/" element={<AnecdoteList anecdotes={anecdotes}/> }/>
+        <Route path="/" element={<div><Notification message={notification}/> <AnecdoteList anecdotes={anecdotes}/></div>}/>
         <Route path="/anecdotes" element={<AnecdoteList anecdotes={anecdotes}/> }/>
-        <Route path="/create-new" element={<CreateNew addNew={addNew}/>}/>
+        <Route path="/create-new" element={notification ? <Navigate replace to='/'/> : <CreateNew addNew={addNew}/>}/>
         <Route path="/anecdotes/:id" element={<Anecdote anecdotes={anecdotes}/> }/>
       </Routes>
       <Footer />
